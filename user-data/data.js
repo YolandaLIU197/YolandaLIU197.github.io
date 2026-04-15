7354926226d99badd9c8034afc48b5ab55d8aa85
YolandaLIU197 <liuyanjun197@gmail.com>
2026-04-14 23:43:40 -0400

    Update user data with new profile information

--- user-data/data.js
+++ user-data/data.js
@@ -1,175 +1,132 @@
 const data ={
   "bio": [
-    "Hi, I'm Vinay Somawat — a Frontend Engineer with 5+ years of experience building scalable, high-performance web applications using Angular and React.",
-    "I specialize in component architecture, performance optimization, and modern frontend tooling like Storybook, Stencil, and Module Federation. I actively leverage GitHub Copilot and OpenAI APIs to accelerate UI development, automate workflows, and improve developer productivity.",
-    "I’m passionate about clean code, mentoring, and driving engineering excellence through collaboration, automation, and continuous improvement."
+    "Hi, I'm Yolanda Liu, a Master's student in Financial Engineering at Columbia University, with an undergraduate background in Business and Information Science at Cornell University.",
+    "I specialize in machine learning, quantitative modeling, and software engineering, with hands-on experience in recommendation systems, explainable AI, and reinforcement learning — building end-to-end data systems that turn complex problems into actionable solutions.",
+    "I thrive in collaborative environments and bring both technical depth and strong communication across research, analytics, and business settings. I'm as comfortable whiteboarding models as I am presenting findings to stakeholders.",
+    "My edge comes from bridging two worlds: the technical precision of an engineer and the strategic thinking of a business background. I care not just about building strong models, but about how they support real decisions and create value for the people I work with."
   ],
-  "skills": [
-    "JavaScript",
-    "TypeScript",
-    "Angular",
-    "AngularJS",
-    "React",
-    "HTML5",
-    "CSS3",
-    "Tailwind CSS",
-    "Material UI",
-    "Bootstrap",
-    "RxJS",
-    "NGXS",
-    "NGRX",
-    "WebSockets",
-    "Module Federation",
-    "Signals API",
-    "Micro-frontend Architecture",
-    "StencilJS",
-    "Storybook",
-    "Webpack",
-    "Git",
-    "GitHub Copilot",
-    "OpenAI APIs",
-    "Chrome DevTools",
-    "Performance Optimization",
-    "Responsive Design",
-    "Web Accessibility",
-    "Unit Testing",
-    "ESLint",
-    "Prettier"
-  ],
-  "experience": [
+"skills": [
+  "<b>Languages</b>",
+  "Python (Pandas, NumPy, Scikit-learn)", "SQL", "R", "JavaScript", "C/C++",
+
+  "<b>Machine Learning & AI</b>",
+  "PyTorch", "TensorFlow", "Graph Neural Networks", "Explainable AI", "Reinforcement Learning", "Deep Q-Learning",
+
+  "<b>Data Engineering & Statistics</b>",
+  "ETL Pipelines", "Bayesian Statistics", "A/B Testing", "Time Series Analysis", "Statistical Testing", "Model Evaluation", "Database Design",
+
+  "<b>Software & Dev Tools</b>",
+  "Git", "GitHub", "YAML", "Unit Testing", "Flask", "REST APIs", "SQLite", "Excel VBA", "JMP",
+
+  "<b>Business & Financial Tools</b>",
+  "Microsoft Excel", "Microsoft Access", "Argus", "S&P Capital IQ", "Bloomberg", "SAP/ERP"
+],
+
+    "experience": [
     {
-      "title": "SS&C Technologies",
-      "duration": "August 2025 - Present",
-      "subtitle": "Senior Software Engineer",
+      "title": "Delonix Group",
+      "duration": "June 2024 - May 2025",
+      "subtitle": "Data Science & Analytics Analyst",
       "details": [
-        "coming soon..."
+        "Engineered an automated data pipeline using Python, SQL, and Excel VBA to track KPIs across a 14-brand portfolio, delivering a real-time analytics dashboard that improved CFO performance review and decision-making efficiency",
+        "Built a Microsoft Access + SQL database to structure Cornell-area student housing data and engineered four relational tables with formatted reports to streamline diligence and identify investment opportunities"
       ],
-      "tags": [],
+      "tags": [
+        "Data Pipeline Automation",
+        "SQL",
+        "Relational Database Design"
+      ],
+      "icon": "house"
+    },
+    {
+      "title": "Bain & Company",
+      "duration": "May 2024 - October 2024",
+      "subtitle": "Digital Transformation Part-Time Assistant",
+      "details": [
+        "Advised a duty-free retail company on digital transformation to modernize their inventory management system ",
+        "Evaluated the SOP of the client’s inventory management, benchmarked against industry peers; conducted in-depth research and recommended digitalization initiatives to enhance operational efficiency and address key improvement areas",
+        "Conducted in-depth research on stock-count processes and produced a case optimizing stock count in SAP/ERP"
+      ],
+      "tags": [
+        "Digital Transformation",
+        "Operational Research",
+        "Process Optimization",
+        "ERP / SAP Process Analysis"
+      ],
+      "icon": "people"
+    },
+    {
+      "title": "Tuck Advisors",
+      "duration": "August 2023 - Janurary 2024",
+      "subtitle": "Investment Banking Intern",
+      "details": [
+        "<b>Select Transaction Experience:</b> $50 Million Buy-Side Mandate for an Ed-Tech Company",
+        "Conducted quantitative target research, utilizing data-driven criteria to benchmark 200+ potential acquisition targets",
+        "Collaborated with senior stakeholders to assess strategic fit and facilitate acquisition decisions through market analysis"
+      ],
+      "tags": [
+        "Investment Banking",
+        "Buy-Side M&A",
+        "Strategic Analysis"
+      ],
       "icon": "money"
-    },
-    {
-      "title": "Shiprocket Limited",
-      "duration": "September 2023 - August 2025",
-      "subtitle": "Software Engineer",
-      "details": [
-        "Led the migration of 30+ high-traffic modules (serving 300K+ active sellers) from AngularJS to Angular 19, conducting in-depth analysis of legacy code, identifying and addressing functional gaps in updated designs, executing testing across multiple user roles, and delivering a seamless rollout in a live production environment.",
-        "Engineered a ChatGPT-style courier rule engine using OpenAPI schema, automating 20+ ops tasks; built global search with deep linking and keyboard shortcuts, boosting efficiency by 30%",
-        "Contributed to the Shiprocket UI Design System using StencilJS + Storybook with atomic design, cutting UI bugs by 35% and halving feature delivery time across teams.",
-        "Integrated GitHub Copilot and ChatGPT into dev workflows, accelerating code generation and debugging; enforced linting pratices and mentored 5+ engineers in modern Angular."
-      ],
-      "tags": [
-        "TypeScript",
-        "Storybook",
-        "GitHub Copilot"
-      ],
-      "icon": "truck"
-    },
-    {
-      "title": "Biofourmis India Pvt. Ltd.",
-      "duration": "April 2022 - July 2023",
-      "subtitle": "Software Engineer",
-      "details": [
-        "Spearheaded micro-frontend adoption with Module Federation, eliminating shared deployment blockers and enabling team- wise independent releases.",
-        "Architected multi-region deployment support with dynamic API routing and failover, improving global latency.",
-        "Integrated real-time collaboration tools (Twilio Video, Countly, NGXS WebSockets) to enable 24/7 patient monitoring and synchronous team interactions."
-      ],
-      "tags": [
-        "TypeScript",
-        "RxJS",
-        "NGXS",
-        "WebSockets",
-        "Module Federation"
-      ],
-      "icon": "heartbeat"
-    },
-    {
-      "title": "Novopay Solutions Pvt. Ltd.",
-      "duration": "July 2020 - April 2022",
-      "subtitle": "Software Engineer",
-      "details": [
-        "Built Aadhaar-enabled banking modules (eKYC, withdrawal, balance inquiry) for 100K+ retail agents, ensuring secure financial access in rural areas.",
-        "Designed QR-code wallet flow, reducing transaction failures and increasing user adoption across merchants.",
-        "Integrated Clevertap, Freshdesk, and chatbot support, cutting average support resolution time by 40%."
-      ],
-      "tags": [
-        "JavaScript",
-        "Angular",
-        "Bootstrap"
-      ],
-      "icon": "qrcode"
-    }
-  ],
+    } 
+    ],
+    
   "education": [
     {
-      "title": "B.Tech. in Computer Science and Engineering",
-      "duration": "2016 - 2020",
-      "subtitle": "National Institute of Technology, Warangal",
+      "title": "M.S. in Financial Engineering",
+      "duration": "2025 - 2026",
+      "subtitle": "Columbia University, New York",
       "details": [],
-      "tags": [],
+      "tags": [
+        "Stochastic Models", 
+        "Applied Algorithmic Research", 
+        "Statistical Analysis and Time Series",
+        "AI Application"
+      ],
       "icon": "graduation-cap"
     },
     {
-      "title": "Class 12th in Science and Mathematics",
-      "duration": "",
-      "subtitle": "Board of Secondary Education, Rajasthan",
-      "details": [],
-      "tags": [],
-      "icon": "book"
+      "title": "B.S. in Business Administration",
+      "duration": "2021 - 2025",
+      "subtitle": "Cornell University, Ithaca",
+      "details": [
+        "GPA: 3.9/4.0",
+        "Minors in Information Science and Real Estate"
+                 ],
+      "tags": [
+        "Quantitative Analysis",
+        "Database Management (SQL)", 
+        "Networks",
+        "Data Science", 
+        "Excel VBA"
+      ],
+      "icon": "graduation-cap"
     }
   ],
   "contactLinks": [
     {
       "label": "Email",
-      "link": "mailto:vinaysomawat40@gmail.com",
+      "link": "mailto:yl6021@columbia.edu",
       "icon": "fa fa-envelope"
     },
     {
       "label": "LinkedIn",
-      "link": "https://www.linkedin.com/in/vinaysomawat/",
+      "link": "https://www.linkedin.com/in/yolandaliu02/",
       "icon": "fa-brands fa-linkedin"
     },
     {
       "label": "GitHub",
-      "link": "https://github.com/vinaysomawat",
+      "link": "https://github.com/YolandaLIU197",
       "icon": "fa-brands fa-github"
-    },
-    {
-      "label": "Buy me a coffee",
-      "link": "https://www.buymeacoffee.com/r194dME8y",
-      "icon": "fa fa-coffee"
     }
   ],
   "footer": [
     {
-      "label": "Links",
-      "data": [
-        {
-          "text": "Buy me a coffee",
-          "link": "https://www.buymeacoffee.com/r194dME8y"
-        },
-        {
-          "text": "Github",
-          "link": "https://github.com/vinaysomawat/vinaysomawat.github.io"
-        }
-      ]
-    },
-    {
-      "label": "Pages",
-      "data": [
-        {
-          "text": "Trekking & Himalayan Pass Adventures",
-          "link": "./pages/adventure.html"
-        },
-        {
-          "text": "Generate JSON data",
-          "link": "./pages/json-generator.html"
-        }
-      ]
-    },
-    {
       "label": "copyright-text",
       "data": [
-        "Built with HTML, CSS, JavaScript, and ❤️"
+        "Yolanda Liu | © 2026"
       ]
     }
   ]
